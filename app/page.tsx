79c79e484fbfffba37f80e5797f8db06d040a3b5
sowon23sowon23-coder <sowon23sowon23@gmail.com>
2026-08-30 16:47:34 +0900

    Open app without login gate

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { AuthGate } from "@/components/AuthGate";
 import { CuringShell } from "@/components/CuringShell";
 
 export default function Home() {
-  return (
-    <AuthGate>
-      <CuringShell />
-    </AuthGate>
-  );
+  return <CuringShell />;
 }
